e8defbbab0e6a78e3f94b6095afa1e1925fb0189
Jan eudald VAQUER <jvaquer@e2r3p1.42.fr>
2020-01-10 17:59:47 +0100

    made all work : parse tr

--- parse/ft_parse_tr.c
+++ parse/ft_parse_tr.c
@@ -6,7 +6,7 @@
 /*   By: jvaquer <jvaquer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/20 18:18:14 by jvaquer           #+#    #+#             */
-/*   Updated: 2020/01/10 17:05:26 by jvaquer          ###   ########.fr       */
+/*   Updated: 2020/01/10 17:59:05 by jvaquer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ int		ft_parse_tr(char *s, int i)
 		g_data->tr->spec = 1;
 	else if (s[i] == 't')
 		i = ft_parse_tr_3(s, i);
-	if (i == -1 || s[i])
+	if (i == -1)
 		return (-1);
 	if (g_data->tr->id == 0)
 		save_tr = g_data->tr;
